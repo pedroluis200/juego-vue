e660a0fff3a2d2ac192366405f37a31df211baa7
Raidel <rraidel85@gmail.com>
2022-07-15 03:31:21 -0700

    Add player special attack

--- lab 15/js/app.js
+++ lab 15/js/app.js
@@ -1,86 +1,89 @@
 new Vue({
-    el: "#app",
-    data: {
-        playerHealth: 600,
-        playerSpecial: 0,
-        monsterHealth: 1000,
-        monsterLoosedLife: 0,
-        maxAttack: 50,
-        gameStarted: false,
-        turns: [],
+  el: "#app",
+  data: {
+    playerHealth: 600,
+    playerSpecial: 0,
+    monsterHealth: 1000,
+    monsterLoosedLife: 0,
+    maxAttack: 50,
+    gameStarted: false,
+    turns: [],
+  },
+  methods: {
+    startGame() {
+      this.playerHealth = 600;
+      this.monsterHealth = 1000;
+      this.playerSpecial = 0;
+      this.gameStarted = true;
+      this.monsterLoosedLife = 0;
+      this.maxAttack = 10;
+      this.turns = [];
     },
-    methods: {
-        startGame() {
-            this.playerHealth = 600;
-            this.monsterHealth = 1000;
-            this.playerSpecial = 0;
-            this.gameStarted = true;
-            this.monsterLoosedLife = 0;
-            this.maxAttack = 10;
-            this.turns = [];
-        },
-        calculateDamage: function(min, max) {
-            return Math.max(Math.floor(Math.random() * max) + 1, min);
-        },
-        attack() {
-            const damage = this.calculateDamage(3, this.maxAttack);
-            this.monsterHealth -= damage;
-            this.logBattle(true, "El jugador ataca al oponente por " + damage);
-            this.monsterAttack();
-        },
-        specialAttack() {
-            const damage = this.calculateDamage(10, 20);
-            this.monsterHealth -= damage;
-            this.logBattle(
-                true,
-                "El jugador usa su ataque especial sobre el oponente por " + damage
-            );
-            this.monsterAttack();
-        },
-        heal() {
-            if (this.playerHealth <= 90) {
-                this.playerHealth += 10;
-            } else {
-                this.playerHealth = 100;
-            }
-            this.logBattle(true, "El jugador recupera 10 puntos de salud");
-            this.monsterAttack();
-        },
-        giveUp() {
-            this.logBattle(true, "El jugador escapa de la batalla");
-            this.gameStarted = false;
-        },
-        monsterAttack() {
-            const damage = this.calculateDamage(30, 50);
-            this.playerHealth -= damage;
-            this.playerSpecial += damage;
-            this.logBattle(false, "El oponente ataca al jugador por " + damage);
-        },
-        notify(message) {
-            if (confirm(message + " ¿Nuevo juego?")) {
-                this.startGame();
-            } else {
-                this.gameStarted = false;
-            }
-        },
-        logBattle(playerTurn, action) {
-            this.turns.unshift({
-                isPlayer: playerTurn,
-                text: action,
-            });
-        },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
-    watch: {
-        playerHealth(newValue) {
-            if (newValue <= 0) {
-                this.notify("¡Has Perdido!");
-            }
-        },
-        monsterHealth(newValue) {
-            this.maxAttack += (1000 - newValue) / 8;
-            if (newValue <= 0) {
-                this.notify("¡Has Ganado!");
-            }
-        },
+    attack() {
+      const damage = this.calculateDamage(3, this.maxAttack);
+      this.monsterHealth -= damage;
+      this.logBattle(true, "El jugador ataca al oponente por " + damage);
+      this.monsterAttack();
     },
+    specialAttack() {
+      if (this.monsterHealth < 500) {
+        this.playerSpecial = 0;
+        this.logBattle(
+          true,
+          "El jugador usa su ataque especial sobre el oponente por 500"
+        );
+        this.monsterHealth -= 500;
+      } else {
+        this.logBattle(true, "El jugador no puede realizar su ataque especial");
+      }
+    },
+    heal() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.logBattle(true, "El jugador recupera 10 puntos de salud");
+      this.monsterAttack();
+    },
+    giveUp() {
+      this.logBattle(true, "El jugador escapa de la batalla");
+      this.gameStarted = false;
+    },
+    monsterAttack() {
+      const damage = this.calculateDamage(30, 50);
+      this.playerHealth -= damage;
+      this.playerSpecial += damage;
+      this.logBattle(false, "El oponente ataca al jugador por " + damage);
+    },
+    notify(message) {
+      if (confirm(message + " ¿Nuevo juego?")) {
+        this.startGame();
+      } else {
+        this.gameStarted = false;
+      }
+    },
+    logBattle(playerTurn, action) {
+      this.turns.unshift({
+        isPlayer: playerTurn,
+        text: action,
+      });
+    },
+  },
+  watch: {
+    playerHealth(newValue) {
+      if (newValue <= 0) {
+        this.notify("¡Has Perdido!");
+      }
+    },
+    monsterHealth(newValue) {
+      this.maxAttack += (1000 - newValue) / 8;
+      if (newValue <= 0) {
+        this.notify("¡Has Ganado!");
+      }
+    },
+  },
 });
